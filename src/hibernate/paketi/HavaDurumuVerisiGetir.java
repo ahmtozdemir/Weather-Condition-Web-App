d6c2923780887c0dfcf487f35d707dd56e393f00
Ahmet Özdemir <32216376+ahmtozdemir@users.noreply.github.com>
2018-08-04 20:13:11 +0300

    refactoring

--- src/hibernate/paketi/HavaDurumuVerisiGetir.java
+++ src/hibernate/paketi/HavaDurumuVerisiGetir.java
@@ -2,59 +2,39 @@ package hibernate.paketi;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class HavaDurumuVerisiGetir
-{
-    public  static List<HavaDurumu> databaseVeriGetir(String sehir)
-    {
+public class HavaDurumuVerisiGetir {
+    public  static List<HavaDurumu> databaseVeriGetir(String sehir) {
         SessionFactory sessionFactory= HavaDurumuSessionFactorySingleton.getSessionFactory();
-
         Session session=sessionFactory.getCurrentSession();
 
         List<HavaDurumu>havaDurumuList=null;
 
-
-
-        try
-        {
+        try {
             havaDurumuList=new ArrayList<>();
             session.beginTransaction();
-
-            havaDurumuList=  session.createQuery("from HavaDurumu  where sehir=?")
-                    .setParameter(0,sehir)
-                    .getResultList();
-
-
+            havaDurumuList=  session.createQuery("from HavaDurumu  where sehir=?").setParameter(0,sehir).getResultList();
             session.getTransaction().commit();
-
-            System.out.println("HavaDurumu bilgilerini liste olarak databaseden aldık agam!");
-
-
+            System.out.println("HavaDurumu bilgileri liste olarak databaseden alındı!");
 
         }
-        catch (Exception e)
-        {
-            System.out.println("Yolladığınız şehre ait hava durumu bulunamadı...");
+        catch (Exception e) {
+            System.out.println("İstediğiniz şehre ait hava durumu bulunamadı...");
             havaDurumuList=null;
         }
-        finally
-        {
+        finally {
             session.close();
         }
-
         return  havaDurumuList;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         List<HavaDurumu>havaDurumuList=new ArrayList<>();
         havaDurumuList= HavaDurumuVerisiGetir.databaseVeriGetir("ankara");
 
-        for(HavaDurumu havaDurumu:havaDurumuList)
-        {
+        for(HavaDurumu havaDurumu:havaDurumuList) {
             System.out.println(havaDurumu);
         }
     }
