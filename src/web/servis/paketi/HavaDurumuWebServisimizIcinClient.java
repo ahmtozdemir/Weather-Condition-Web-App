1025aa73a7690ddce521b190d83241d31ebd46a2
Ahmet Özdemir <32216376+ahmtozdemir@users.noreply.github.com>
2018-09-02 12:39:55 +0300

    refactoring

--- src/web/servis/paketi/HavaDurumuWebServisimizIcinClient.java
+++ src/web/servis/paketi/HavaDurumuWebServisimizIcinClient.java
@@ -5,7 +5,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -13,8 +12,7 @@ import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HavaDurumuWebServisimizIcinClient
-{
+public class HavaDurumuWebServisimizIcinClient {
     public static List<HavaDurumu> bizimWebServistenVeriCek(String sehirAdi) throws ParseException {
         Client client= ClientBuilder.newClient();
         WebTarget target=client.target("http://localhost:8080/service/weather/city?sehir="+sehirAdi);
@@ -23,8 +21,7 @@ public class HavaDurumuWebServisimizIcinClient
         JSONParser jsonParser = new JSONParser();
         JSONArray anaObject = (JSONArray) jsonParser.parse(target.request(MediaType.APPLICATION_JSON).get(String.class) );
 
-        for (int i=0;i<13;i++)
-        {
+        for (int i=0;i<13;i++) {
             JSONObject gunObjesi = (JSONObject) anaObject.get(i);
             String sehir = (String) gunObjesi.get("sehir");
             String durum = (String) gunObjesi.get("durum");
@@ -35,19 +32,15 @@ public class HavaDurumuWebServisimizIcinClient
 
             HavaDurumu havaDurumu = new HavaDurumu(sehir, durum, sicaklik, basinc, nem, zaman);
             havaDurumuList.add(havaDurumu);
-
         }
         return havaDurumuList;
-
     }
-
 
     public static void main(String[] args) throws ParseException {
 
         List<HavaDurumu>havaDurumuList=new ArrayList<>();
         havaDurumuList= HavaDurumuWebServisimizIcinClient.bizimWebServistenVeriCek("ANKARA");
-        for(HavaDurumu havaDurumu:havaDurumuList)
-        {
+        for(HavaDurumu havaDurumu:havaDurumuList) {
             System.out.println(havaDurumu);
         }
     }
