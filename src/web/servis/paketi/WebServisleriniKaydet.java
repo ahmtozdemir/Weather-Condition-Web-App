dca2bf58aa1eb0cc6b5c3850c280cccffe186207
Ahmet Özdemir <32216376+ahmtozdemir@users.noreply.github.com>
2018-09-02 12:40:43 +0300

    Update WebServisleriniKaydet.java

--- src/web/servis/paketi/WebServisleriniKaydet.java
+++ src/web/servis/paketi/WebServisleriniKaydet.java
@@ -2,22 +2,13 @@ package web.servis.paketi;
 
 import java.util.HashSet;
 import java.util.Set;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-
-
 @ApplicationPath("/service")
-
-//@ApplicationPath("/") OK
-//@ApplicationPath("/*") Don't use /*
-//@ApplicationPath("/root-path/*") Don't use /*
-
-public class WebServisleriniKaydet extends Application{
+public class WebServisleriniKaydet extends Application {
 
     private Set<Object> singletons = new HashSet<Object>();
-
     public WebServisleriniKaydet() {
         singletons.add(new DatabaseHavaDurumuWebService());
         singletons.add(new DatabaseAylikSicaklikWebService());
